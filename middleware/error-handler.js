f8c9757971c2ddaa185e6e97d07e1f366a7253af
BoxunW <tony1572002@gmail.com>
2023-08-21 13:23:33 +0800

    feat: add api error handler & delete restaurant api

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // eslint-disable-next-line space-before-function-paren
   generalErrorHandler(err, req, res, next) {
     if (err instanceof Error) {
       req.flash('error_messages', `${err.name}: ${err.message}`)
@@ -7,6 +6,20 @@ module.exports = {
       req.flash('error_messages', `${err}`)
     }
     res.redirect('back')
+    next(err)
+  },
+  apiErrorHandler(err, req, res, next) {
+    if (err instanceof Error) {
+      res.status(err.status || 500).json({
+        status: 'error',
+        message: `${err.name}: ${err.message}`
+      })
+    } else {
+      res.status(500).json({
+        status: 'error',
+        message: `${err}`
+      })
+    }
     next(err)
   }
 }
