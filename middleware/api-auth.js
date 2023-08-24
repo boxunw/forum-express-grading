4a6ac484ac605e658c1c9d882fe4752633fba728
BoxunW <tony1572002@gmail.com>
2023-08-24 16:35:21 +0800

    feat: add user to req user

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -2,7 +2,7 @@ const passport = require('../config/passport') // 引入 passport
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
-
+    req.user = user // 驗證成功後將 user 放入 req.user
     next()
   })(req, res, next)
 }
